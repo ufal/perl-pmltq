838355624e58357fba8354c4628dcbe251a663b1
Matyáš Kopp <matyas.kopp@gmail.com>
2015-09-06 17:50:05 +0200

    added man command

--- lib/PMLTQ/Command/man.pm
+++ lib/PMLTQ/Command/man.pm
@@ -1,9 +1,25 @@
+=head1 SYNOPSIS
+
+  pmltq man
+
+=head1 DESCRIPTION
+
+Print whole help.
+
+=cut
 package PMLTQ::Command::man;
 use PMLTQ::Command;
+use PMLTQ::Command::help;
 
 sub run {
   my $self = shift;
-  print "VERSION";
+  PMLTQ::Command::help->run(undef,1);
+  my @modules = PMLTQ::Command::module_list();
+  my %commands = map {my $key = $_; $key =~ s/^PMLTQ::Command:://;($key => $_)} @modules;
+  for my $cmd (keys %commands){
+    print STDERR "\n=== $cmd ===\n";
+    PMLTQ::Command::help->run($cmd,1);
+  }
 }
 
 
