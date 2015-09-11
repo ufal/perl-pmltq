85b9679ffeb832f8d73edbe7ddcff71deae4bb97
Matyáš Kopp <matyas.kopp@gmail.com>
2015-09-11 17:49:46 +0200

    fixing problem with DB handler when initializing database

--- lib/PMLTQ/Command/initdb.pm
+++ lib/PMLTQ/Command/initdb.pm
@@ -16,11 +16,11 @@ use PMLTQ::Command;
 sub run {
   my $self = shift;
   my $config = PMLTQ::Command::load_config(shift);
-  $dbh = PMLTQ::Command::db_connect($config,'postgres');
+  my $dbh = PMLTQ::Command::db_connect($config,'postgres');
   $dbh->do("CREATE DATABASE ".$config->{db}->{name});
   $dbh = PMLTQ::Command::db_connect($config);
   for my $file (qw/init_postgres.sql pml2base_init-pg.sql/) {
-    PMLTQ::Command::run_sql_from_file($file,File::Spec->catfile(PMLTQ->shared_dir,"sql"));
+    PMLTQ::Command::run_sql_from_file($file,File::Spec->catfile(PMLTQ->shared_dir,"sql"), $dbh);
   }
   PMLTQ::Command::db_disconnect($dbh);
 }
