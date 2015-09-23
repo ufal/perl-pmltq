10ced4722c3b50a6b51fdf487e12db2bc34bda97
Matyáš Kopp <matyas.kopp@gmail.com>
2015-09-23 16:03:42 +0200

    fixed path to config file in database manipulation test support script

--- t/scripts/postgres_config.sh
+++ t/scripts/postgres_config.sh
@@ -1,6 +1,6 @@
 
 
-configfile='../treebanks/pdt20_sample_small/config.yml'
+configfile="$(dirname "$0")/../treebanks/pdt20_sample_small/config.yml"
 user=`grep '^\s*user:' $configfile|head -n1 | sed  -e 's/^\s*user:\s*"\?\([^"\s]*\)"\?/\1/'`
 pass=`grep '^\s*password:' $configfile|head -n1 | sed  -e 's/^\s*password:\s*"\?\([^"\s]*\)"\?/\1/'`
 port=`grep '^\s*port:' $configfile|head -n1 | sed  -e 's/^\s*port:\s*"\?\([^"\s]*\)"\?/\1/'`
