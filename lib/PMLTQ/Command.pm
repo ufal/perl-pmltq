2203bfb700ccb899f92032bde5c9a28daf05bcbb
Matyáš Kopp <matyas.kopp@gmail.com>
2017-01-24 14:29:51 +0100

    Build results of fe79031 (on master)

--- lib/PMLTQ/Command.pm
+++ lib/PMLTQ/Command.pm
@@ -1,5 +1,6 @@
 package PMLTQ::Command;
-
+our $AUTHORITY = 'cpan:MATY';
+$PMLTQ::Command::VERSION = '1.3.2';
 # ABSTRACT: Command base class
 
 use PMLTQ::Base -base;
@@ -325,3 +326,48 @@ sub evaluate_query {
   return $result;
 }
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+PMLTQ::Command - Command base class
+
+=head1 VERSION
+
+version 1.3.2
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Petr Pajas <pajas@ufal.mff.cuni.cz>
+
+=item *
+
+Jan Štěpánek <stepanek@ufal.mff.cuni.cz>
+
+=item *
+
+Michal Sedlák <sedlak@ufal.mff.cuni.cz>
+
+=item *
+
+Matyáš Kopp <matyas.kopp@gmail.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Institute of Formal and Applied Linguistics (http://ufal.mff.cuni.cz).
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
